d6bb62997eecb1962f3724fec37b4a1f34da135f
Коноплев Сергей Николаевич <snkonoplev@mts.ru>
2022-04-30 17:50:16 +0300

    Improve select method

--- convertions.go
+++ convertions.go
@@ -1,7 +1,7 @@
 package list
 
 func Select[A, B any](l List[A], mapper func(a A) B) List[B] {
-	var result List[B]
+	var result = make(List[B], 0, len(l))
 
 	for _, a := range l {
 		result = append(result, mapper(a))
